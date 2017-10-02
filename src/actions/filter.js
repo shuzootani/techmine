44a0221410f656ebe339aee8b311cba7027c8c4e
shuzootani <suzo.yolo@gmail.com>
2017-10-02 12:53:30 +0900

    Updates

--- src/actions/filter.js
+++
@@ -1,6 +0,0 @@
-export const filterByTab = (tab) => {
-    return {
-        type: 'FILTER_BY_TAB',
-        tab
-    }
-}
--- src/utils/filter.js
+++
@@ -1,3 +0,0 @@
-export const filterFeeds = (feeds, keyword) => {
-    return feeds.filter(feed => feed.title.toLowerCase().search(keyword.toLowerCase()) >= 0)
-}
